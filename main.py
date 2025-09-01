c5a25825591094b2fa348a1fb46c7685507ea631
fedesegala <federico.segala@equixly.com>
2025-09-01 21:21:33 +0200

    modified main to use correct momentums for plots

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-from optimized_neural_network.neural_network import NeuralNetwork
+import drawdata
+
+from optimized_neural_network.neural_network import NeuralNetwork as OptimizedNeuralNetwork
 from sklearn.datasets import load_iris, load_digits, fetch_openml
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 import numpy as np
 
-def main():
+def compare_with_toy_datasets(toy_name = "digits"):
     # print("Downloading MNIST dataset")
     # mnist = fetch_openml('mnist_784', version=1, as_frame=False)
     # X, Y = np.array(mnist["data"], dtype=np.float64), mnist["target"].astype(int)
     # X /= 255.0
 
-    data = load_digits()
+    if toy_name == "digits":
+        data = load_digits()
+    else:
+        data = load_iris()
     X = data.data
     Y = data.target
-    Y = data.target
 
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
 
     print(f"x_train shape: {x_train.shape}")
     # DIGITS
-    nn_momentum = NeuralNetwork.initialize(
+    nn_momentum = OptimizedNeuralNetwork.initialize(
         input_size=x_train.shape[-1],
-        hidden_layer_sizes=[50,50,50],
+        hidden_layer_sizes=[50,50] if toy_name == "digits" else [5],
         output_size=len(np.unique(y_test)),
-        lr=0.02,
-        momentum=0.5,
+        lr=0.01,
+        momentum=0.75 if toy_name == "digits" else 0.8,
     )
 
     nn_momentum.train(
@@ -37,11 +41,11 @@ def main():
         compute_stats_interval=1
     )
 
-    nn_non_momentum = NeuralNetwork.initialize(
+    nn_non_momentum = OptimizedNeuralNetwork.initialize(
         input_size=x_train.shape[-1],
-        hidden_layer_sizes=[50,50,50],
+        hidden_layer_sizes=[50,50] if toy_name == "digits" else [5],
         output_size=len(np.unique(y_test)),
-        lr=0.02,
+        lr=0.01,
         momentum=0
     )
 
@@ -60,10 +64,65 @@ def main():
     plot_comparison(
         momentum_stats,
         non_momentum_stats,
-        f"Model: lr{nn_momentum.lr}, alpha{nn_momentum.momentum}",
-        f"Model: lr{nn_momentum.lr}, alpha{nn_non_momentum.momentum}",
+        f"Model: lr{nn_momentum.lr}, alpha: {nn_momentum.momentum}",
+        f"Model: lr{nn_momentum.lr}, alpha: {nn_non_momentum.momentum}",
 
     )
+
+def compare_with_mnist():
+    print("Downloading MNIST dataset")
+    mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+    X, Y = np.array(mnist["data"], dtype=np.float64), mnist["target"].astype(int)
+    X /= 255.0
+
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.7, random_state=42)
+
+    print(f"x_train shape: {x_train.shape}")
+
+    nn_momentum = OptimizedNeuralNetwork.initialize(
+        input_size=x_train.shape[-1],
+        hidden_layer_sizes=[50, 50],
+        output_size=len(np.unique(y_test)),
+        lr=0.4,
+        momentum=0.9
+    )
+
+    nn_momentum.train(
+        epochs=30,
+        x_train=x_train,
+        x_test=x_test,
+        y_train=y_train,
+        y_test=y_test,
+        compute_stats_interval=1
+    )
+
+    nn_non_momentum = OptimizedNeuralNetwork.initialize(
+        input_size=x_train.shape[-1],
+        hidden_layer_sizes=[50, 50],
+        output_size=len(np.unique(y_test)),
+        lr=0.4,
+        momentum=0
+    )
+
+    nn_non_momentum.train(
+        epochs=30,
+        x_train=x_train,
+        x_test=x_test,
+        y_train=y_train,
+        y_test=y_test,
+        compute_stats_interval=1
+    )
+
+    momentum_stats = nn_momentum.stats
+    non_momentum_stats = nn_non_momentum.stats
+
+    plot_comparison(
+        momentum_stats,
+        non_momentum_stats,
+        f"Model: lr{nn_momentum.lr}, alpha: {nn_momentum.momentum}",
+        f"Model: lr{nn_momentum.lr}, alpha: {nn_non_momentum.momentum}",
+    )
+
 
 def plot_comparison(stats1, stats2, title1, title2):
     epochs = [s['epoch'] for s in stats1]
@@ -116,4 +175,5 @@ def plot_comparison(stats1, stats2, title1, title2):
 
 if __name__ == "__main__":
     np.random.seed(42)
-    main()
+    compare_with_toy_datasets("digits")
+    # compare_with_mnist()
